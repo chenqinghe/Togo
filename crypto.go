3fcc517fa498e900ee1a2fec5961d718d7e33747
Chen qinghe <chenqh721@foxmail.com>
2017-02-08 23:38:01 +0800

    Add files via upload

---
+++ crypto.go
@@ -0,0 +1,15 @@
+package pfunc
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func Md5(s string) (string, error) {
+	h := md5.New()
+	if _, err := h.Write([]byte(s)); err != nil {
+		return "", err
+	}
+	result := h.Sum(nil)
+	return string(result), nil
+}
--- strings.go
+++ strings.go
@@ -1,33 +1,59 @@
 package pfunc
 
 import (
-
-
+	"fmt"
+	"strings"
 )
 
-
-func Explode(s1,s2 string)([]string,error){
-  
+func Explode(str, sep string) ([]string, error) {
+	if sep == "" {
+		return []string{str}, nil
+	}
+	return strings.Split(str, sep), nil
 }
 
-func Hex2bin(s string)(string,string){
+func Hex2bin(s string) (string, error) {
+	return "", nil
+}
 
+func HtmlSpecialCharsDecode(s string) (string, error) {
+	return "", nil
+}
+
+func HtmlSpecialChars(s string) string {
+	s = strings.Replace(s, "<", "&lt;", -1)
+	s = strings.Replace(s, ">", "&gt;", -1)
+	return s
 
 }
 
-
-
-func HtmlSpecialCharsDecode(s string)(string,error){
-
+func Trim(s1, s2 string) string {
+	return strings.Trim(s1, s2)
 }
 
-func htmlSpecialChars(s string)(string,error){
-
+func Ltrim(s1, s2 string) string {
+	return strings.TrimLeft(s1, s2)
 }
 
+func Rtrim(s1, s2 string) string {
+	return strings.TrimRight(s1, s2)
+}
 
-func Ltrim(s1,s2 string)(string error){
-
-
-} 
-
+func Reverse(s string) string {
+	if s == "" {
+		return s
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	length := len([]rune(s))
+	var re []rune = make([]rune, length)
+	i := 0
+	for _, v := range s {
+		re[length-i-1] = v
+		i++
+	}
+	return string(re)
+}
