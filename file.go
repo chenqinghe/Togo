b6a63efebb4c7a85a0e8ea391bbf4ced0bfc7ec0
chenqinghe <chenqh721@foxmail.com>
2017-03-01 21:50:38 +0800

    add function chmod chdir

--- file.go
+++ file.go
@@ -11,3 +11,38 @@ func BaseName(path, suffix string) (string, error) {
 	}
 	return fi.Name(), nil
 }
+
+func Chdir(dir string) error {
+	return os.Chdir(dir)
+}
+
+//func Chgrp(filename string,group interface{})error{
+//	uid := os.Geteuid()
+//	switch group.(type) {
+//	case string:
+//		grp := os.
+//	case int:
+//		return os.Chown(filename,uid,group.(int))
+//	default:
+//		return errors.New("unsupported group type")
+//
+//	}
+//	os.Chown()
+//}
+
+func Chmod(filename string, mode int) error {
+	return os.Chmod(filename, os.FileMode(mode))
+}
+
+
+//func Chown(filename string,user interface{})error{
+//	switch user.(type) {
+//		case string:
+//			grp := os.
+//		case int:
+//			return os.Chown(filename,uid,group.(int))
+//		default:
+//			return errors.New("unsupported user type")
+//		}
+//	return os.Chown()
+//}
